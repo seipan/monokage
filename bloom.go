cc0e77b9a544a4bbbbcfcc3daeb5f6c04d1d15ad
seipan <yamasakipann0218@gmail.com>
2023-11-21 05:09:39 +0900

    feat(bloom) : Add TestAndAdd Method

--- bloom.go
+++ bloom.go
@@ -41,6 +41,25 @@ func (bf *bloomFilter) Test(key []byte) bool {
 	return true
 }
 
+func (bf *bloomFilter) TestAndAdd(key []byte) bool {
+	var result bool
+	result = true
+	hash := getMD5Hash(key)
+	hashA := hash[:int(len(hash)/2)]
+	hashB := hash[int(len(hash)/2):]
+
+	i64_hashA, _ := strconv.ParseInt(hashA, 16, 64)
+	i64_hashB, _ := strconv.ParseInt(hashB, 16, 64)
+
+	for i := uint(0); i < bf.k; i++ {
+		if bf.b.Test(uint(doubleHashing(int64(i64_hashA), int64(i64_hashB), int(i), int(bf.m)))) {
+			result = false
+		}
+		bf.b.Set(uint(doubleHashing(int64(i64_hashA), int64(i64_hashB), int(i), int(bf.m))))
+	}
+	return result
+}
+
 func newBloomFilter(m uint, k uint) *bloomFilter {
 	return &bloomFilter{
 		m: m,
